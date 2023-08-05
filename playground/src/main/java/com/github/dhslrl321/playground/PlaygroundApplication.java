955318f2a18dfbf5c2c0a41f58a491ace1c19dab
dhslrl321 <dhslrl321@gmail.com>
2023-08-05 22:30:36 +0900

    commit

--- playground/src/main/java/com/github/dhslrl321/playground/PlaygroundApplication.java
+++ playground/src/main/java/com/github/dhslrl321/playground/PlaygroundApplication.java
@@ -2,6 +2,7 @@ package com.github.dhslrl321.playground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PlaygroundApplication {
@@ -9,5 +10,14 @@ public class PlaygroundApplication {
     public static void main(String[] args) {
         SpringApplication.run(PlaygroundApplication.class, args);
     }
+
+    @Bean
+    A a() {
+        return new A();
+    }
+
+}
+
+class A {
 
 }
